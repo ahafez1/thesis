93538347dbc2c218192db777ff17111ac789a190
A. Hafez <ahafez1@aucegypt.edu>
2020-03-06 22:39:34 +0200

    added money calculation

--- src/app/dev1/dev1.page.ts
+++ src/app/dev1/dev1.page.ts
@@ -11,13 +11,11 @@ import { NavController } from '@ionic/angular';
     templateUrl: './dev1.page.html',
     styleUrls: ['./dev1.page.scss'],
 })
-export class Dev1Page {
+export class Dev1Page implements OnInit{
 
     chartData: ChartDataSets[] = [{ data: [], label: 'Power Consumption' }];
     chartLabels: Label[];
-
-    // Options
-    //chartLabels = [ 'time', 'others'];
+    public money: string;
 
     chartOptions = {
         responsive: true,
@@ -43,9 +41,7 @@ export class Dev1Page {
     chartType = 'line';
     showLegend = true;
 
-    // For search
-
-    timeframe = '30';
+    timeframe = '1';
 
     constructor(private http: HttpClient) {
     }
@@ -58,6 +54,7 @@ export class Dev1Page {
                 //this.chartData = data as any[];	 // FILL THE CHART ARRAY WITH DATA.
                 console.log('res: ', result);
                 const powerdata = result;
+                let money = '0';
 
                 this.chartLabels = [];
                 this.chartData[0].data = [];
@@ -65,39 +62,17 @@ export class Dev1Page {
                 for (const entry of powerdata[Symbol.iterator]()) {
                     this.chartLabels.push(entry.Time);
                     this.chartData[0].data.push(entry.Current);
+                    money = String(parseInt(money) + parseInt(entry.Current));
                 }
+                console.log('money: ', money);
+                this.money = money;
             },
 
         );
         console.log('data: ', this.chartData);
     }
-
 
     ngOnInit() {
-        var query = String(this.timeframe);
-        var uri = 'https://thesis2020.000webhostapp.com/api/specificTime.php?Device=1&Time=' + encodeURIComponent(query);
-        this.http.get(uri).subscribe(
-            result => {
-                //this.chartData = data as any[];	 // FILL THE CHART ARRAY WITH DATA.
-                console.log('res: ', result);
-                const powerdata = result;
-
-                this.chartLabels = [];
-                this.chartData[0].data = [];
-
-                for (const entry of powerdata[Symbol.iterator]()) {
-                    this.chartLabels.push(entry.Time);
-                    this.chartData[0].data.push(parseInt(entry.Current));
-                }
-            },
-        );
-        console.log('data: ', this.chartData);
-        //this.getData();
-    }
-
-
-    typeChanged(e) {
-        const on = e.detail.checked;
-        this.chartType = on ? 'line' : 'bar';
+        this.getData();
     }
 }
